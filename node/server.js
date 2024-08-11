1cbcf06415bdf66b9cdb802c3c2d99bc858937c1
Clebson Moura <clebson.sh@gmail.com>
2024-08-11 18:38:05 -0300

    better code organization to avoid running npm install everytime my code changes

--- node/server.js
+++ node/server.js
@@ -3,7 +3,7 @@ import Fastify from 'fastify'
 const fastify = Fastify({ logger: true })
 
 fastify.get('/', async (_, reply) => {
-  reply.send({ hello: 'world' })
+  reply.send({ hello: 'world!' })
 })
 
 fastify.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
